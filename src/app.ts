dc2bb7e1bc897113a1995dd97e5b75ae343c1691
bbworld1 <vwangsf@gmail.com>
2019-12-12 15:57:40 -0800

    Make app.ts load tokens from ENV

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import { requireFile, logger, BotConfig, Bot } from './resources';
 
 let config: BotConfig = requireFile('./bot-config.json');
 
+// Add tokens and API Keys via ENV (needed for heroku)
+config.discord.token = process.env.BOT_TOKEN;
+config.youtube.apikey = process.env.YOUTUBE_KEY;
+
 let bot = new Bot(config);
 
 bot.connect()
